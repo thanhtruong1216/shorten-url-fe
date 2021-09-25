0b4ed81744b0bd4263046b67ee85a35402d87696
Thanh Truong <thanhtruong1216@gmail.com>
2021-09-25 14:05:21 +0700

    feat: create and show list of shorten urls

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios"
+import "./App.css"
+import "antd/dist/antd.css"
+import { Button, Input, message, Table } from "antd"
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons"
+import { useState, useEffect } from "react"
+
+const columns = [
+  {
+    title: "Shorten Url",
+    dataIndex: "shorten_url",
+    key: "shorten_url",
+  },
+  {
+    title: "Action",
+    render: () => {
+      return (
+        <div style={{ display: "flex" }}>
+          <div style={{ paddingRight: "1rem" }}>
+            <EditOutlined />
+          </div>
+          <div>
+            <DeleteOutlined />
+          </div>
+        </div>
+      )
+    },
+  },
+]
 
 function App() {
+  const [url, setUrl] = useState("")
+  const [shortenUrls, setShortenUrls] = useState([])
+
+  const createLink = () => {
+    axios({
+      method: "post",
+      url: "http://localhost:3000/links",
+      data: { link: { url } },
+    })
+      .then(res => {
+        console.log("rs", res)
+        if (res.data && res.data.status === 200) {
+          message.success("Shorten Url generated")
+        } else {
+          message.error("Generate Shorten Url Failed")
+        }
+      })
+      .catch(() => message.error("Something went wrong"))
+  }
+
+  const handleUrlValue = e => setUrl(e.target.value)
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "http://localhost:3000/links",
+    }).then(res => setShortenUrls(res.data.result))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: "5rem" }}>
+      <div style={{ display: "flex" }}>
+        <Input placeholder="Basic usage" onChange={handleUrlValue} />
+        <Button type="primary" onClick={createLink}>
+          Create link
+        </Button>
+      </div>
+
+      <Table dataSource={shortenUrls} columns={columns} rowKey="id" />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
